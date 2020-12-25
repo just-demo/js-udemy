c00fb86415f2983e14a77f8b9c58456a4f6f476d
Anatolii Kryvda <anatolii.kryvda@automationanywhere.com>
2020-12-25 16:06:57 +0200

    Support different representations of input course id

--- review-dates.js
+++ review-dates.js
@@ -1,10 +1,10 @@
 const request = require('sync-request');
+const path = require('path');
 
-const course = process.argv[2];
-
+const courseId = getCourseId();
 const dates = [];
 let total = null;
-let url = `https://www.udemy.com/api-2.0/courses/${course}/reviews/?page=1`;
+let url = `https://www.udemy.com/api-2.0/courses/${courseId}/reviews/?page=1`;
 while (url) {
     let progress = total ? Math.round(100 * dates.length / total) : 0;
     console.log(url, `${progress}%`);
@@ -16,6 +16,39 @@ while (url) {
 
 dates.sort();
 
+console.log('Course id:', courseId);
 console.log('Number of reviews:', dates.length);
 console.log('First review date:', dates[0]);
 console.log('Last review date:', dates[dates.length - 1]);
+
+function getCourseId() {
+    if (process.argv.length < 3) {
+        console.log('Please provide path to course with a program argument:');
+        console.log(`node ${path.basename(__filename)} <cource-url|course-literal-id|course-numeric-id>`);
+        process.exit(1);
+    }
+
+    let courseId = process.argv[2];
+    if (/^\d+$/.test(courseId)) {
+        return courseId;
+    }
+
+    let courseUrl = courseId.includes('/course/') ? courseId :
+        'https://www.udemy.com/course/' + courseId.replace(/^\/+|\/+$/g, '');
+
+    let courseHtml;
+    try {
+        courseHtml = request('GET', courseUrl).getBody('utf8');
+    } catch (error) {
+        console.log('Error reading page:', courseUrl);
+        process.exit(1);
+    }
+
+    let match = /data-clp-course-id="(\d+)"/.exec(courseHtml);
+    if (!match) {
+        console.log('Error fetching course id from page:', courseUrl);
+        process.exit(1);
+    }
+
+    return match[1];
+}
